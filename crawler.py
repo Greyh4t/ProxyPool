fd876048ae0d1eaf23c820dea0fe8a342cec23e9
Greyh4t <Greyh4t@users.noreply.github.com>
2016-08-30 18:29:27 +0800

    fix bugs
    fix bugs

--- crawler.py
+++ crawler.py
@@ -14,16 +14,6 @@ from proxysites import get_proxy_sites
 CRAWLER_POOL = Pool(CRAWLER_CONFIG['THREAD_NUM'])
 
 
-def exception_handler(func):
-    def handler(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except:
-            return []
-
-    return handler
-
-
 class Crawler(object):
     def run(self):
         proxy_sites = self._get_proxy_sites()
@@ -33,19 +23,22 @@ class Crawler(object):
         logger.info('Get %s proxies' % len(proxies))
         return proxies
 
-    @exception_handler
     def crawl(self, site):
         site_url = site[0]
         pattern = site[1]
         proxies = []
-        r = requests.get(site_url, headers=HEADER, timeout=CRAWLER_CONFIG['TIMEOUT'])
+        r = None
+        try:
+            r = requests.get(site_url, headers=HEADER, timeout=CRAWLER_CONFIG['TIMEOUT'])
+        except:
+            pass
         COUNT = 0
         while CRAWLER_CONFIG['RETRY_TIMES'] > COUNT:
-            if (not r.ok) or len(r.content) < 500:
+            if not r or (not r.ok) or len(r.content) < 500:
                 port = API_CONFIG['PORT']
-                r = requests.get('http://localhost:%s/?type=3' % port)
+                pr = requests.get('http://localhost:%s/?type=3' % port)
                 try:
-                    proxy = json.loads(r.content)
+                    proxy = json.loads(pr.content)
                     if proxy:
                         proxy = proxy[0]
                         r = requests.get(site_url, headers=HEADER, timeout=CRAWLER_CONFIG['TIMEOUT'],
@@ -58,7 +51,7 @@ class Crawler(object):
                 COUNT += 1
             else:
                 break
-        if not r.ok:
+        if not r or not r.ok:
             return []
         for match in pattern.finditer(r.content):
             ip = match.groupdict()['ip']
